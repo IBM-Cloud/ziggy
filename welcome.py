3df07c2237bdf987aa936c5ce706e72a55c8cfb4
Matt Eldridge <matt.eldridge@us.ibm.com>
2016-02-09 16:42:50 -0500

    Add Cloudant and Personality Insights, remove cruft routes, get insights on personas.

--- welcome.py
+++ welcome.py
@@ -13,44 +13,68 @@
 # limitations under the License.
 
 import os
-import urllib
-
+import json
 from flask import Flask, jsonify
-from rauth import OAuth1Service
-from bs4 import BeautifulSoup
+from cloudant.account import Cloudant
+from watson_developer_cloud import PersonalityInsightsV2 as PersonalityInsights
 
+if 'VCAP_SERVICES' not in os.environ:
+    raise RuntimeError("VCAP_SERVICES not found.")
+elif 'cloudantNoSQLDB' not in json.loads(os.environ['VCAP_SERVICES']):
+    raise RuntimeError("Cloudant database not bound to service.")
+
+WATSON = json.loads(os.environ['VCAP_SERVICES'])['personality_insights'][0]
+if 'credentials' not in WATSON:
+    raise RuntimeError("Cannot connect to Watson.  Credentials not found for personality insights.")
+else:
+    personality_insights = PersonalityInsights(username=WATSON['credentials']['username'], password=WATSON['credentials']['password'])
+
+CLOUDANT = json.loads(os.environ['VCAP_SERVICES'])['cloudantNoSQLDB'][0]
+if 'credentials' not in CLOUDANT:
+    raise RuntimeError("Cannot connect to database, Cloudant credentials not found.")
+else:
+    client = Cloudant(CLOUDANT['credentials']['username'], CLOUDANT['credentials']['password'], url=CLOUDANT['credentials']['url'])
+    client.connect()
+
+databases = ['personas', 'albums', 'songs']
+for db in databases:
+    if db not in client.all_dbs():
+        raise RuntimeError("Database " + db + " not found, please ensure you have the needed data.")
+
+def assemble_persona_text(persona):
+    text = ''
+    for album in client['personas'][persona]['albums']:
+        for song in client['albums'][album]['songs']:
+            if 'lyrics' in client['songs'][song]:
+                text += client['songs'][song]['lyrics']
+    return text
+
+## Begin Flask server
 app = Flask(__name__)
+if 'FLASK_DEBUG' in os.environ:
+    app.debug = True
 
 @app.route('/')
 def Welcome():
     return app.send_static_file('index.html')
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
-
 @app.route('/setup')
 def Setup():
-    
-    r = urllib.urlopen('http://www.azlyrics.com/lyrics/davidbowie/littlewonder.html').read()
-    soup = BeautifulSoup(r)
-    print soup.find_all('br')
-    return 'Gathering data ...'
+    return 'Setup complete.'
 
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
 
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
+@app.route('/api/personas')
+def GetPersonas():
+    response = []
+    for persona in client['personas']:
+        response.append({'name': persona['_id'], 'albums': persona['albums']})
+
+    return jsonify(results=response)
+
+@app.route('/api/persona/<persona>')
+def GetPersona(persona):
+    insight = personality_insights.profile(json.dumps({'text': assemble_persona_text(persona), 'contenttype': 'text/html'}))
+    return jsonify(results=insight)
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
